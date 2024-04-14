f8cf5cf06509aadf65d1074c5565a3c7557e7673
NatrayanS <s.natrayan06ayan96@gmail.com>
2024-04-14 19:53:38 +0530

    api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fitnesstrack-6ydw.onrender.com",
+  baseURL: "https://fitnesstrack-6ydw.onrender.com/api",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
